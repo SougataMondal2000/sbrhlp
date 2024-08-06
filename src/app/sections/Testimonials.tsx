71d1627a7f6c6b058c9286dae52be646af7e3589
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-08-06 10:28:03 +0530

    testimonial changes

--- src/app/sections/Testimonials.tsx
+++ src/app/sections/Testimonials.tsx
@@ -1,4 +1,4 @@
-import TesttimonialCard from "../components/TesttimonialCard";
+import TestimonialCard from "../components/TestimonialCard";
 
 const Testimonials = () => {
   return (
@@ -7,31 +7,33 @@ const Testimonials = () => {
         Well wishes from our Patrons
       </h3>
       <div
-        className="lg:h-[600px] h-fit"
+        className="lg:h-[600px] h-fit flex lg:justify-center lg:items-center "
         style={{
           backgroundImage:
             "url('https://images.unsplash.com/photo-1528459801416-a9e53bbf4e17?q=80&w=2024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         }}
       >
-        <div className="flex overflow-x-scroll lg:gap-2 flex-wrap justify-center items-center h-full">
-          <TesttimonialCard
-            name="Anna Stokes"
-            title="Artist"
-            imageSrc="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            rating={4}
-          />
-          <TesttimonialCard
-            name="Anna Stokes"
-            title="Artist"
-            imageSrc="https://images.unsplash.com/photo-1583341612074-ccea5cd64f6a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            rating={4}
-          />
-          <TesttimonialCard
-            name="Anna Stokes"
-            title="Artist"
-            imageSrc="https://images.unsplash.com/photo-1445053023192-8d45cb66099d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            rating={4}
-          />
+        <div className="flex lg:justify-center lg:items-center overflow-x-auto snap-x snap-mandatory py-4 px-2 md:px-4 w-full scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-thumb-slate-700 scrollbar-track-slate-300 ">
+          <div className="flex gap-4 w-max">
+            <TestimonialCard
+              name="Anna Stokes"
+              title="Artist"
+              imageSrc="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              rating={4}
+            />
+            <TestimonialCard
+              name="Anna Stokes"
+              title="Artist"
+              imageSrc="https://images.unsplash.com/photo-1445053023192-8d45cb66099d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              rating={4}
+            />
+            <TestimonialCard
+              name="Anna Stokes"
+              title="Artist"
+              imageSrc="https://images.unsplash.com/photo-1445053023192-8d45cb66099d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              rating={4}
+            />
+          </div>
         </div>
       </div>
     </div>
